47e81b24ec9844bce215dac6dbf82e57f5acd835
arinto <arinto@otnira.com>
2012-10-14 20:20:08 +0200

    update the code with RW and RWRW calculation

--- friends.py
+++ friends.py
@@ -3,6 +3,28 @@
 import urllib2
 import re
 import random
+import signal
+import sys
+
+def sigquitHandler(signum, frame):
+    print "\nage avg = " + str(total_age / counter_age)
+    print "age rwrw avg = " + str(total_rwrw_num_age / total_rwrw_denum_age)
+    
+    print "playlist avg = " + str(total_playlists / counter_total)
+    print "playlist rwrw avg = " + str(total_rwrw_num_playlists / total_rwrw_denum)
+    
+    print "playcount avg = " + str(total_playcounts / counter_total)
+    print "playcount rwrw avg = " + str(total_rwrw_num_playcounts / total_rwrw_denum)
+    
+    print "id avg = " + str(total_id / counter_total)
+    print "id rwrw avg = " + str(total_rwrw_num_id / total_rwrw_denum)
+    
+    print "degree avg = " + str(total_friends / counter_total)
+    print "degree rwrw avg = " + str(total_rwrw_num_friends/ total_rwrw_denum)
+    sys.exit(0)
+	
+signal.signal(signal.SIGINT, sigquitHandler)
+signal.siginterrupt(signal.SIGINT, True)
 
 #init some variables
 command_limit_one= \
@@ -10,64 +32,95 @@ command_limit_one= \
 "&user={0}&limit=1&page={1}&api_key={2}"
 
 command_user_info = \
-"http://ws.audioscrobbler.com/2.0/?method=user.getinfo"\
-"&user={0}&api_key={1}"
+    "http://ws.audioscrobbler.com/2.0/?method=user.getinfo"\
+    "&user={0}&api_key={1}"
 
 command_user_shouts = \
-"http://ws.audioscrobbler.com/2.0/?method=user.getshouts"\
-"&user={0}&limit={1}&api_key={2}"
+    "http://ws.audioscrobbler.com/2.0/?method=user.getshouts"\
+    "&user={0}&limit={1}&api_key={2}"
 
 api_key = 'cda9140cf81af12206d411e1d420af18' #team_amz's API Key
 user = "rj"
-num_users = 100
-x = 0
 
-#banned_user = "RomanRy" #or "robolover"
-#user = banned_user
-#command = command_user_info.format(banned_user, api_key)
-#data = urllib2.urlopen(command).read()
-#print data
+#app counters
+total_age = 0
+counter_age = 0
+total_friends = 0
+total_playcounts = 0
+total_playlists = 0
+total_id = 0
+counter_total = 0
 
-#command = command_user_shouts.format(banned_user, 5, api_key)
-#data = urllib2.urlopen(command).read()
-#print data
+total_rwrw_num_age = 0.0
+total_rwrw_num_friends = 0.0
+total_rwrw_num_playcounts = 0.0
+total_rwrw_num_playlists = 0.0
+total_rwrw_num_id = 0.0
+total_rwrw_denum_age = 0.0
+total_rwrw_denum = 0.0
 
-#command = command_user_info.format(user, api_key)
-#data = urllib2.urlopen(command).read()
-#print data
-
-#team_amz = "team_amz"
-#command = command_user_info.format(team_amz, api_key)
-#data = urllib2.urlopen(command).read()
-#print data
-
-
-#crawl num_users Lastfm users
-while (x < num_users):
-
-    command = command_limit_one.format(user, 1, api_key)
+while (1):
+	#INFO COMMAND________________________________________________
+    data = ''
+    command = command_user_info.format(user, api_key)
     data = urllib2.urlopen(command).read() # data is in XML format
-    
-    #learn the degree
-    degree = int(re.search('total="(\d+)"', data).group(1)) 
-    id = random.randint(0, degree-1)
-	
-    #get the data for random user
-    command = command_limit_one.format(user, id, api_key)
-    data = urllib2.urlopen(command).read()
 
-    #print data
-
-    #useful data : number of friends, age, playcount, playlists, id
-    friends = re.findall("<name>(.*)</name>", data)	
     ages = re.findall("<age>(.*)</age>",data)		
     playcounts = re.findall("<playcount>(.*)</playcount>", data)
     playlists = re.findall("<playlists>(.*)</playlists>", data)
     friend_ids = re.findall("<id>(.*)</id>", data)
+    
+    #update totals________________________________________________
+    total_playcounts += int(playcounts[0])
+    total_playlists += int(playlists[0])
+    total_id += int(friend_ids[0])
+    
+    #update counters______________________________________________
+    counter_total += 1
+    if ages[0] is not '':
+        counter_age += 1
+        total_age += int(ages[0])
 
-    print "I am", user, "and from",degree,"friends, I picked",friends[0],\
-    "index", id, "friend_id", friend_ids[0],"age", ages[0], "playcount", \
-    playcounts[0], "playlists", playlists[0]
+	#DEGREE COMMAND_______________________________________________
+    data = ''	
+    command = command_limit_one.format(user, 1, api_key)
+    data = urllib2.urlopen(command).read() # data is in XML format
+    
+    #add degree
+    degree = int(re.search('total="(\d+)"', data).group(1)) 
+    total_friends += degree
+
+    #update rwrw_________________________________________________
+    total_rwrw_num_friends += (degree/degree)
+    total_rwrw_num_playcounts += float(playcounts[0])/degree 
+    total_rwrw_num_playlists += float(playlists[0])/degree
+    total_rwrw_num_id += float(friend_ids[0])/degree
+
+    total_rwrw_denum += 1.0/degree
+
+    if ages[0] is not '':
+        total_rwrw_num_age += float(ages[0])/degree
+        total_rwrw_denum_age += 1.0/degree
+
+    #friend id
+    id = random.randint(1, degree)
+	
+
+    #FRIENDS COMMAND_____________________________________________
+    
+    #get the data for random user
+    data = ''
+    command = command_limit_one.format(user, id, api_key)
+    data = urllib2.urlopen(command).read()
+
+    #useful data : number of friends, age, playcount, playlists, id
+    friends = re.findall("<name>(.*)</name>", data)	
+
+    #log activity______________________________________________
+    if counter_age > 0 :
+        print "[counter_total, counter_age] = ["+str(counter_total)+","\
+        +str(counter_age)+"] : [playcount,playlist,id,degree,age] = ["\
+        +str(total_playcounts)+","+str(total_playlists)+","\
+        +str(total_id)+","+str(total_friends)+","+str(total_age)+"]"
 
     user = friends[0]
-    x+=1
